c02ae9e7d06179db1a7862d8bca9ef9f91366358
Huynh Tien <huynhqtienvtag@gmail.com>
2021-09-11 11:15:50 +0700

    better target catch

--- src/main/java/me/hsgamer/villagedefenseextras/fix/DefenseTargetFix.java
+++ src/main/java/me/hsgamer/villagedefenseextras/fix/DefenseTargetFix.java
@@ -1,12 +1,20 @@
 package me.hsgamer.villagedefenseextras.fix;
 
 import me.hsgamer.villagedefenseextras.Utils;
+import org.bukkit.Location;
+import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import plugily.projects.villagedefense.arena.Arena;
 import plugily.projects.villagedefense.creatures.CreatureUtils;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class DefenseTargetFix implements Listener {
     @EventHandler
@@ -18,9 +26,18 @@ public class DefenseTargetFix implements Listener {
             return;
         }
 
-        if (Utils.isInArena(entity.getLocation()) && !CreatureUtils.isEnemy(target)) {
-            event.setTarget(null);
-            event.setCancelled(true);
+        Optional<Arena> optionalArena = Utils.getArena(entity.getLocation());
+        if (optionalArena.isPresent() && !CreatureUtils.isEnemy(target)) {
+            List<Creature> list = optionalArena.get().getEnemies();
+            Location location = entity.getLocation();
+            Optional<Creature> optionalTarget = list.stream()
+                    .filter(c -> Objects.equals(c.getLocation().getWorld(), location.getWorld()))
+                    .min(Comparator.comparingDouble(c -> c.getLocation().distance(location)));
+            if (optionalTarget.isPresent()) {
+                event.setTarget(optionalTarget.get());
+            } else {
+                event.setCancelled(true);
+            }
         }
     }
 }
